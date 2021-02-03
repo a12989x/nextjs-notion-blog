6960d8c40ae846622820ce44c4bed569bd6cab42
Alexis Guzman <alexis12989x@gmail.com>
2021-02-03 12:53:52 -0600

    Create Head component

---
+++ components/Head.js
@@ -0,0 +1,21 @@
+import NextHead from 'next/head';
+
+const Head = () => {
+    return (
+        <NextHead>
+            <meta charSet='UTF-8' />
+            <link rel='icon' href='' />
+            <title>Mi Blog | Codax</title>
+            <meta
+                name='description'
+                content='Amas el desarrollo web? Conoce los principles temas relacionados y tips que te ayudaran a ser un mejor desarrollador web.'
+            />
+            <meta
+                name='viewport'
+                content='width=device-width, initial-scale=1'
+            />
+        </NextHead>
+    );
+};
+
+export default Head;
