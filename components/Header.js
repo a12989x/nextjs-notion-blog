3cef2727bae1b3a0851f8d051b873b8ce62c1ac0
Alexis Guzman <alexis12989x@gmail.com>
2021-02-04 11:55:18 -0600

    Set up Header component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Header = () => {
     return (
-        <div>
-            <p>Header Component</p>
-        </div>
+        <header className='header'>
+            <Link href='/'>
+                <a className='header__logo'>
+                    <Image
+                        src='/assets/svg/logo.min.svg'
+                        height='30'
+                        width='30'
+                    />
+                    Codax
+                </a>
+            </Link>
+
+            <ul className='header__links'>
+                <li className='header__item'>
+                    <Link href='/about'>
+                        <a className='header__link'>About</a>
+                    </Link>
+                </li>
+                <li className='header__item'>
+                    <Link href='/portfolio'>
+                        <a className='header__link'>Portfolio</a>
+                    </Link>
+                </li>
+            </ul>
+        </header>
     );
 };
 
