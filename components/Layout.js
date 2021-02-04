6acbb9062cfcf3348b8f32f3a4c5da8c0fbe454f
Alexis Guzman <alexis12989x@gmail.com>
2021-02-04 11:55:28 -0600

    Import Header component

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,12 @@
 import Head from './Head';
+import Header from './Header';
 import Footer from './Footer';
 
 const Layout = ({ children }) => {
     return (
         <>
             <Head />
+            <Header />
             {children}
             <Footer />
         </>
