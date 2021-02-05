114754fe3d4cf04a4af301eec6a7fc9c44325919
Alexis Guzman <alexis12989x@gmail.com>
2021-02-05 13:42:49 -0600

    Import About component

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,10 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import About from '../components/About';
 import Posts from '../components/Posts';
 
 const Home = () => {
     return (
         <section className='home'>
-            <div className='home__images'>
-                <Image
-                    className='home__cover'
-                    src='/assets/cover.png'
-                    alt='cover'
-                    height='353'
-                    width='1000'
-                />
-                <Image
-                    className='home__avatar'
-                    src='/assets/avatar.png'
-                    alt='avatar'
-                    height='121'
-                    width='120'
-                    quality='100'
-                />
-            </div>
-            <h1 className='home__title'>Codax</h1>
-            <p className='home__description'>
-                Hello world 👋
-                <br /> I'm <b className='home__name'>Alexis Guzman</b>, a
-                freelance web developer who has fun building websites like{' '}
-                <br />
-                <Link href='https://ax-covid-19-tracker.web.app/'>
-                    <a
-                        className='home__link'
-                        target='_about'
-                        rel='noopener noreferrer'
-                    >
-                        Covid Tracker
-                    </a>
-                </Link>{' '}
-                y{' '}
-                <Link href='https://ax-sayer-palette.herokuapp.com/'>
-                    <a
-                        className='home__link'
-                        target='_about'
-                        rel='noopener noreferrer'
-                    >
-                        Sayer Palette
-                    </a>
-                </Link>
-            </p>
-
-            <Link href='/portfolio'>
-                <a className='home__seeMyWork'>
-                    <button className='home__btn'>See my work</button>
-                </a>
-            </Link>
+            <About />
 
             <hr className='home__hr' />
 
